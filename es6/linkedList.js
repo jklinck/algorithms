81cd816ae9019372666cdfb8b715a9bf5647866d
jklinck <joeklinck@hotmail.com>
2016-07-06 14:17:24 -0700

    added size() and prepend() to es6/linkedList.js

--- es6/linkedList.js
+++ es6/linkedList.js
@@ -1,9 +1,26 @@
+let cl = (input) => console.log(input);
+
 class linkedList{
 	constructor(){
 		this.head = null;
 	}
 	isEmpty(){
 		return this.head === null;
+	}
+	// size of list
+	size(){
+		var count = 0;
+		if(this.isEmpty()){
+			return count;
+		}
+		else{
+			var current = this.head;
+			while(current){
+				count++;
+				current = current.next;
+			}
+		}
+		return count;
 	}
 	// append a node
 	append(val){
@@ -22,21 +39,34 @@ class linkedList{
 			current.next = node;
 		}
 	} 
-
+	// prepend a node
+	prepend(val){
+		let node = {
+			data: val,
+			next:null
+		};
+		if(this.isEmpty()){
+			this.head = node;
+		}
+		else{
+			var current = this.head;
+			this.head = node;
+			this.head.next = current;
+		}
+	}
 	// print the linkedList
 	print(){
 		let output = [];
 		if(this.isEmpty()){
-			console.log(output);
+			cl(output);
 		}
 		let current = this.head;
 		while(current){
 			output.push(current.data);
 			current = current.next;
 		}
-		console.log(output);
+		cl(output);
 	} 
-
 } // end of class linkedList
 
 
@@ -44,7 +74,10 @@ const link = new linkedList();
 link.append(5);
 link.append(6);
 link.append(7);
+link.prepend(4);
 link.print();
+
+
 
 
 
